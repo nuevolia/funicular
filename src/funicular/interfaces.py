43db3b64e8f1e330da4e1b4cb025d9c041d508a6
Fabien ZARIFIAN <fabien.zarifian@nuevolia.fr>
2015-08-13 00:29:12 +0200

    Begin browser interfaces

---
+++ src/funicular/interfaces.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""""
+funicular.interfaces - Define interfaces on this package
+"""
+
+
+class IBase(class):
+    pass
+
+
+class IBrowser(IBase):
+    pass
+
+
+class IBrowserProfiles(IBase):
+    pass
+
+
+class IBrowserProfile(IBase):
+    pass
+
+
+class IBrowserExtensions(IBase):
+    pass
+
+
+class IBrowserExtension(IBase):
+    pass
+
+
+class IBrowserConnector(IBase):
+    pass
